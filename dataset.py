06f9c474f80d7b852ebb33d77bb94c90c4dafc83
sjulee <sjulielee@gmail.com>
2023-04-06 14:17:03 -0400

    dataset

--- dataset.py
+++ dataset.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import math
 
 def loadData(dataname):
     if dataname == 'breast-cancer':
@@ -27,3 +28,17 @@ def loadSpam():
     df = pd.read_csv('data/spambase.data', header=None, delimiter=',')
     x, y = df.iloc[:,:-1], df.iloc[:,-1]
     return np.array(x), np.array(y)
+
+def bootstrap_data(seed, ratio, x, y):
+    n = len(y)
+    n_pos = math.ceil(n / (ratio + 1))
+    n_neg = n - n_pos
+
+    rng = np.random.default_rng(seed=seed)
+    ind_pos = rng.choice(np.where(y == 1)[0], n_pos)
+    ind_neg = rng.choice(np.where(y == 0)[0], n_neg)
+
+    x_new = np.vstack((x[ind_pos, :], x[ind_neg, :]))
+    y_new = np.concatenate((np.repeat(1, n_pos), np.repeat(0, n_neg)))
+
+    return x_new, y_new
--- outline.py
+++ outline.py
@@ -1,12 +1,26 @@
 import dataset
+from imblearn.over_sampling import SMOTE
+from imblearn.ensemble import BalancedBaggingClassifier
 
 # Datasets to work on
 datasets = ['breast-cancer', 'spambase']
+ratios = [100, 50, 10, 5, 1]
+B = 1 # Number of bootstraps to do
 
 # For each dataset: Conduct bootstrap stratified sampling with following ratios: 0.01, 0.05, 0.1, 0.2, 0.5
 # Each of these will serve as our dataset
 for data in datasets:
     x, y = dataset.loadData(data)
+
+    for ratio in ratios:
+        for b in range(B):
+            x_new, y_new = dataset.bootstrap_data(b, ratio, x, y)
+
+
+            smote_bagging = BalancedBaggingClassifier(sampler=SMOTE())
+cv_results = cross_validate(smote_bagging, X, y, scoring="balanced_accuracy")
+
+print(f"{cv_results['test_score'].mean():.3f} +/- {cv_results['test_score'].std():.3f}")
 
 # Then try the following:
 # Upsampling of the minority class
