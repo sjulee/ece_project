b33df61b7b9ee4a0fa1057b722aec66c1dfc0ee8
sjulee <sjulielee@gmail.com>
2023-04-06 11:19:04 -0400

    outline

---
+++ outline.py
@@ -0,0 +1,12 @@
+import dataset
+
+# Import datasets
+
+# For each dataset: Conduct bootstrap stratified sampling with following ratios: 0.01, 0.05, 0.1, 0.2, 0.5
+# Each of these will serve as our dataset
+
+# Then try the following:
+# Upsampling of the minority class
+# Downsampling of the majority class
+# AdaCost
+# SMOTEBoost
