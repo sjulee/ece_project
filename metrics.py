67075b4108cff4b49070c879357e4a17d516d47d
Pandey <hpandey30@lawn-143-215-60-240.lawn.gatech.edu>
2023-04-06 17:26:39 -0400

    metrics done

--- metrics.py
+++ metrics.py
@@ -1,30 +1,35 @@
-from sklearn.metrics import *
 import numpy as np
+from sklearn.metrics import *
 
-metrics = ['AUC', 'Accuracy','F-1 Score', 'Average Accuracy', 'Kappa Statistic', 'Precision',
-           'Recall','MCC', 'Balanced Accuracy']
+METRICS = ['AUC', 'Accuracy', 'F-1 Score', 'Kappa Statistic', 'Precision',
+           'Recall', 'MCC', 'Balanced Accuracy']
+
 def get_metrics(y_true, y_pred):
+    score = np.nan()
     y_pred_class = np.round(y_pred)
-    out_metrics = np.zeros(len(metrics))
-    for metric_index in range(len(metrics)):
+    out_metrics = np.zeros(len(METRICS))
+
+    for metric_index in range(len(METRICS)):
         metric = metrics[metric_index]
         if metric == 'AUC':
-            metric_value = auc_m(y_true, y_pred)
+            score = roc_auc_score(y_true, y_pred)
         elif metric == 'Accuracy':
+            score = accuracy_score(y_true, y_pred_class)
+        elif metric == 'F-1 Score':
+            score = f1_score(y_true, y_pred_class)
+        elif metric == 'Balanced Accuracy':
+            score = balanced_accuracy_score(y_true, y_pred_class)
+        elif metric == 'Kappa Statistic':
+            score = cohen_kappa_score(y_true, y_pred_class)
+        elif metric == 'Precision':
+            score = precision_score(y_true, y_pred_class)
+        elif metric == 'Recall':
+            score = recall_score(y_true, y_pred_class)
+        elif metric == 'MCC':
+            score = matthews_corrcoef(y_true, y_pred_class)
+        else:
+            print("Metric aint available!!!")
 
-
-
-        out_metrics[metric_index] = metric_value
+        out_metrics[metric_index] = score
 
     return out_metrics
-
-def auc_m(y_true, y_pred):
-    score = roc_auc_score(y_true, y_pred)
-    return score
-
-def accuracy_m(y_true, y_pred):
-    score = accuracy_score()
-    return score
-
-def f_1_score_m(y_true, y_pred):
-    return score
